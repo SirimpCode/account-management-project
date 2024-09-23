b60b1d78055842b5e240fc0ca2ac8fb4a8285dde
Sirimp <sihu89@daum.net>
2024-09-23 19:50:28 +0900

    fix: 엔티티 필드값 및 캐스케이딩 수정

--- src/main/java/com/github/accountmanagementproject/web/controller/authAccount/AuthController.java
+++ src/main/java/com/github/accountmanagementproject/web/controller/authAccount/AuthController.java
@@ -1,7 +1,6 @@
 package com.github.accountmanagementproject.web.controller.authAccount;
 
 
-import com.github.accountmanagementproject.repository.redis.RedisTokenRepository;
 import com.github.accountmanagementproject.service.authAccount.SignUpLoginService;
 import com.github.accountmanagementproject.service.customExceptions.CustomBadRequestException;
 import com.github.accountmanagementproject.web.dto.accountAuth.AccountDto;
@@ -9,12 +8,7 @@ import com.github.accountmanagementproject.web.dto.accountAuth.LoginRequest;
 import com.github.accountmanagementproject.web.dto.accountAuth.TokenDto;
 import com.github.accountmanagementproject.web.dto.response.CustomErrorResponse;
 import com.github.accountmanagementproject.web.dto.response.CustomSuccessResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.jasypt.encryption.StringEncryptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
