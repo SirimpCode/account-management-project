4f503333830d4bc1874fdb2c937c0b3883641b31
Sirimp <sihu89@daum.net>
2025-01-09 00:51:54 +0900

    refactor: Swagger 설정 수정 각 프로파일마다 다르게 적용되도록 만듦

--- src/main/java/com/github/accountmanagementproject/config/properties/swagger/SwaggerDev.java
+++ src/main/java/com/github/accountmanagementproject/config/properties/swagger/SwaggerDev.java
@@ -1,6 +1,8 @@
 package com.github.accountmanagementproject.config.properties.swagger;
 
+import com.github.accountmanagementproject.config.properties.server.ServerUrlProperties;
 import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -8,11 +10,13 @@ import java.util.List;
 
 @Profile("dev")
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerDev extends SwaggerProperties {
+    private final ServerUrlProperties serverUrlProperties;
     @Override
     protected List<Server> createServers() {
         Server baseServer = new Server()
-                .url("http://localhost:8080")
+                .url(serverUrlProperties.getBase())
                 .description("Base Server");
         return List.of(baseServer);
     }
