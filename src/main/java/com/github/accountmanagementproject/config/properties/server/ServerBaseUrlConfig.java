cd8c2dc4c610d6a90ab421760637d7d77fadb8ef
Sirimp <sihu89@daum.net>
2025-01-09 14:08:24 +0900

    test: https 요청이 http로 들어오는지 내부 로깅 테스트

--- src/main/java/com/github/accountmanagementproject/config/properties/server/ServerBaseUrlConfig.java
+++ src/main/java/com/github/accountmanagementproject/config/properties/server/ServerBaseUrlConfig.java
@@ -10,7 +10,9 @@ public class ServerBaseUrlConfig {
     @Bean
     public Function<ServerUrlFields, String> requestBaseUrlProvider() {
         return fields -> {
+
             String scheme = fields.getScheme();   // "http" 또는 "https"
+            System.out.println("찍힘 : "+scheme);
             String serverName = fields.getServerName(); // "localhost" 또는 "sirimp.kro.kr"
             int serverPort = fields.getPort(); // 8080 또는 443 등
 
